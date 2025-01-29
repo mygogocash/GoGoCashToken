5350b91b6d7b05ced533aac8010c31b169892af3
Kunanon Jarat <54348660+KunanonJ@users.noreply.github.com>
2025-01-29 21:05:50 +0700

    1st Push

---
+++ GoGoCashToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+//** Aiden Token */
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+/// @title GoGoCash Token
+/// @author GoGoCash.co
+/// @dev A token based on OpenZeppelin's principles
+
+contract AidenToken is ERC20Burnable {
+    /// @notice A constructor that mint the tokens
+    constructor() ERC20("GoGoCash", "GGC") {
+        _mint(msg.sender, 100_000_000 * 10 ** decimals());
+    }
+}
